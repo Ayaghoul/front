6cc9e4a71b6582e25474a1901d9ab2c3a643172d
Ayaghoul <eyalghoul@gmail.com>
2025-08-21 20:21:44 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import ErrorBoundary from "./components/ErrorBoundary";
 
-function App() {
+import HomePage from "./pages/HomePage";
+import ProductsPage from "./pages/ProductsPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import CartPage from "./pages/CartPage";
+import AdminProductsPage from "./pages/AdminProductsPage";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <ErrorBoundary>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/admin/products" element={<AdminProductsPage />} />
+          <Route path="/admin" element={<h1>👑 Espace Admin</h1>} />
+          <Route path="/account" element={<h1>👤 Espace Client</h1>} />
+        </Routes>
+      </ErrorBoundary>
+    </>
   );
-}
+};
 
 export default App;
